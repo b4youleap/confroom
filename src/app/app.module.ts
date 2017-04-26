5843e8a1458a816d52022983dacf65c1c0399b4a
Bruce MacDonald <Bruce MacDonald>
2017-04-26 13:15:18 -0700

    initial commit with material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+
+import { MdButtonModule, MdCheckboxModule } from '@angular/material';
+import 'hammerjs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,8 +16,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
+    MdButtonModule,
+    MdCheckboxModule,
     AppRoutingModule
   ],
   providers: [],
